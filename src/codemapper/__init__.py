3513ddcefa611135f40eac3a2a6daecef0146dde
Shane Holloman <shaneholloman@gmail.com>
2024-10-03 02:54:47 +1300

    Update version to 3.7.0, expose wip: add package info retrieval module, and add spreadsheet to Markdown converter

--- src/codemapper/__init__.py
+++ src/codemapper/__init__.py
@@ -6,6 +6,6 @@ This package provides functionality to analyze local directories or GitHub repos
 creating detailed Markdown documentation of their structure and contents.
 """
 
-__version__ = "3.6.0"
+__version__ = "3.7.0"
 
 # Any other necessary imports or package-level code can go here
